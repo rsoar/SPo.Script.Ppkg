93f91177e5a1f817ee83639cc8f56cbd96a06a2a
Rafael Medeiros <77183668+rsoar@users.noreply.github.com>
2022-11-10 23:20:46 -0300

    Add files via upload

---
+++ deployment.ps1
@@ -0,0 +1,18 @@
+﻿$package = "spfx-deployment-automation-web.sppkg"
+$solutionJSON = "$(Get-Location)\config\package-solution.json"
+$solutionPath = "$(Get-Location)\sharepoint\solution\"
+$backupPath = "$(Get-Location)\DEPLOY\backup\"
+
+try {
+  set NODE_OPTIONS=--max_old_space_size=8192
+  gulp clean
+  gulp bundle --ship --max_old_space_size=8192
+  gulp package-solution --ship
+  $pkgSolution = Get-Content $solutionJSON | Out-String | ConvertFrom-Json
+  New-Item -Path ("{0}{1}" -f $backupPath,$pkgSolution.solution.version) -ItemType Directory
+  Copy-Item ("{0}{1}" -f $solutionPath,$package) -Destination ("{0}{1}" -f $backupPath,$pkgSolution.solution.version)
+  invoke-item $solutionPath
+}
+catch [System.Net.WebException] {
+  Write-Output "Deploy fails"
+}
